4ee57be0a7509a93b2830d3e232c4c9724580c13
raghu9189 <raghu.ballu@gmail.com>
2024-09-24 21:08:38 +0530

    Learning Java core concepts

--- ReturnNum.java
+++ ReturnNum.java
@@ -1,6 +1,6 @@
-package test;
-import java.util.*;
-public class scratch_2 {
+import java.util.Vector;
+import java.util.Scanner;
+public class ReturnNum {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         char c;
